98c7cf914251d26377996bcde9aea981a9732611
tardelli.moura <tardelli.moura@iteris.com.br>
2019-05-31 17:34:46 -0300

    improvements for file reading

--- src/main/java/br/com/tardelli/utils/file/FlatFileParser.java
+++ src/main/java/br/com/tardelli/utils/file/FlatFileParser.java
@@ -1,12 +1,10 @@
 package br.com.tardelli.utils.file;
 
+import br.com.tardelli.mapper.LogLapMapper;
 import br.com.tardelli.model.LogLap;
-import br.com.tardelli.model.Pilot;
 import br.com.tardelli.utils.file.exception.FlatFileParserException;
 
 import java.io.BufferedReader;
-import java.math.BigDecimal;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -14,11 +12,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
-import static br.com.tardelli.utils.file.RegexUtils.*;
-
 public class FlatFileParser {
-
-  private static final String DEFAULT_HOUR_MIN = "00:0";
 
   private final Logger logger = Logger.getLogger(this.getClass().getName());
 
@@ -27,26 +21,10 @@ public class FlatFileParser {
     Stream<String> stream = bufferedReader.lines();
 
     AtomicBoolean initialLine = new AtomicBoolean(true);
-    stream.forEach(s -> {
-
+    stream.forEach(line -> {
       if(!initialLine.get()){
         try {
-
-          String hourLog = regexMatcher(s, REGEX_HOUR_LOG);
-          String pilotId = regexMatcher(s, REGEX_PILOT_ID);
-          String pilotName = regexMatcher(s, REGEX_PILOT_NAME);
-          String lapTime = regexMatcher(s, REGEX_LAP_TIME);
-          String lapNumber = regexMatcher(s, REGEX_LAP_NUMBER);
-          String averageSpeed = regexMatcher(s, REGEX_AVERAGE_SPEED);
-
-          LogLap logLap = new LogLap();
-          logLap.setHourLog(LocalTime.parse(hourLog));
-          logLap.setPilot(new Pilot(pilotId, pilotName));
-          logLap.setLapNumber(Integer.parseInt(lapNumber));
-          logLap.setAverageSpeed(new BigDecimal(averageSpeed.replace(",", ".")));
-          logLap.setLapTime(LocalTime.parse(DEFAULT_HOUR_MIN + lapTime));
-
-          logLaps.add(logLap);
+          logLaps.add(LogLapMapper.map(line));
         } catch (FlatFileParserException e) {
           logger.log(Level.WARNING, e.getMessage());
         }
--- src/main/java/br/com/tardelli/utils/file/RegexUtils.java
+++ src/main/java/br/com/tardelli/utils/file/RegexUtils.java
@@ -5,20 +5,20 @@ import br.com.tardelli.utils.file.exception.FlatFileParserException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class RegexUtils {
+public class RegexUtils {
 
-  final static String REGEX_HOUR_LOG = "(\\d{2}):(\\d{2}):(\\d{2}).(\\d{3})";
-  final static String REGEX_PILOT_ID = "( \\d{3} )";
-  final static String REGEX_PILOT_NAME = "([a-zA-Z][.][a-zA-Z]+)";
-  final static String REGEX_LAP_NUMBER = "(( \\d{1} )|( \\d{1}\t))";
-  final static String REGEX_LAP_TIME = "(( \\d{1}:\\d{2}.\\d{3})|(\t\t\\d{1}:\\d{2}.\\d{3}))";
-  final static String REGEX_AVERAGE_SPEED = "(\\d{2},(?:\\d{3}|\\d{2}))";
+  public final static String REGEX_HOUR_LOG = "(\\d{2}):(\\d{2}):(\\d{2}).(\\d{3})";
+  public final static String REGEX_PILOT_ID = "( \\d{3} )";
+  public final static String REGEX_PILOT_NAME = "([a-zA-Z][.][a-zA-Z]+)";
+  public final static String REGEX_LAP_NUMBER = "(( \\d{1} )|( \\d{1}\t))";
+  public final static String REGEX_LAP_TIME = "(( \\d{1}:\\d{2}.\\d{3})|(\t\t\\d{1}:\\d{2}.\\d{3}))";
+  public final static String REGEX_AVERAGE_SPEED = "(\\d{2},(?:\\d{3}|\\d{2}))";
 
-  static String regexMatcher(String s, String regex) throws FlatFileParserException {
+  public static String regexMatcher(String s, String regex) throws FlatFileParserException {
     Matcher matcher = Pattern.compile(regex).matcher(s);
     if (matcher.find()) {
       return matcher.group().trim();
     }
-    throw new FlatFileParserException("Erro ao converter o arquivo, linha: " + s);
+    throw new FlatFileParserException("Error converting file, line:" + s);
   }
 }
